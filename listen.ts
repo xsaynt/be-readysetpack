29ad2bfcb8c0d0e854eb2fc5c0107699d18e3d56
Onuryilmaz24 <onurr.yilmaz24@gmail.com>
2025-01-17 15:22:54 +0000

    Update Listen

--- listen.ts
+++ listen.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 
-const {PORT = 9090} = process.env;
+/* const {PORT = 4000} = process.env; */
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT,()=>{
     console.log(`Listening port ${PORT}`)
