a90d8cd32ea1cf0726d5d09b1fa1f9daa441a226
Onuryilmaz24 <onurr.yilmaz24@gmail.com>
2025-01-17 15:16:30 +0000

    Update Connection

--- db/connection.ts
+++ db/connection.ts
@@ -1,7 +1,8 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
 const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({
+dotenv.config({
 	path: `${__dirname}/../.env.${ENV}`,
 });
 
