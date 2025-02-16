4c05bb761e9256deff8586b24caf132ff8a268d9
xsaynt <alex-price3@hotmail.co.uk>
2025-02-16 15:49:49 +0000

    reverted seed changes

--- src/db/seeds/seed.ts
+++ src/db/seeds/seed.ts
@@ -1,6 +1,16 @@
 import db from '../connection';
 import format from 'pg-format';
-import { Users, Trips, Checklist, DailyExpectedCost } from '../../types/types';
+import {
+	Users,
+	Trips,
+	Checklist,
+	DailyExpectedCost,
+	Destination,
+	Weather,
+	Budget,
+	Landmarks,
+	Events,
+} from '../../types/types';
 
 const seed = ({
 	usersData,
