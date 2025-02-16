3d45db73b1a6ea8e78cf32d43236de4c6b9e87db
xsaynt <alex-price3@hotmail.co.uk>
2025-02-16 14:59:16 +0000

    removed development-data info

--- src/db/data/development-data/checklist.ts
+++ src/db/data/development-data/checklist.ts
@@ -1,48 +1,5 @@
 import { Checklist } from '../../../types/types';
 
-const checklistData: Checklist[] = [
-	{
-		trip_id: 1,
-		user_id: 1,
-		items: [
-			'Check your passport',
-			'Print or download your tickets (flight/train/bus).',
-			'Pack comfortable T-shirts/tops.',
-			'Dont forget your pants/shorts/skirts.',
-			'Pack comfortable shoes for walking.',
-			'Pack your toothbrush and toothpaste.',
-			'Bring your phone charger.',
-			'Pack a power bank for emergencies.',
-		],
-	},
-	{
-		trip_id: 2,
-		user_id: 2,
-		items: [
-			'Check your passport',
-			'Print or download your tickets (flight/train/bus).',
-			'Pack comfortable T-shirts/tops.',
-			'Dont forget your pants/shorts/skirts.',
-			'Pack comfortable shoes for walking.',
-			'Pack your toothbrush and toothpaste.',
-			'Bring your phone charger.',
-			'Pack a power bank for emergencies.',
-		],
-	},
-	{
-		trip_id: 3,
-		user_id: 1,
-		items: [
-			'Check your passport',
-			'Print or download your tickets (flight/train/bus).',
-			'Pack comfortable T-shirts/tops.',
-			'Dont forget your pants/shorts/skirts.',
-			'Pack comfortable shoes for walking.',
-			'Pack your toothbrush and toothpaste.',
-			'Bring your phone charger.',
-			'Pack a power bank for emergencies.',
-		],
-	},
-];
+const checklistData: Checklist[] = [];
 
 export default checklistData;
