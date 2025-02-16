5541ec1753b00d82b621d47f68ff40a5a241e55f
xsaynt <alex-price3@hotmail.co.uk>
2025-02-16 15:51:19 +0000

    readded dev data

--- src/db/data/development-data/dailycost.ts
+++ src/db/data/development-data/dailycost.ts
@@ -1,5 +1,22 @@
 import { DailyExpectedCost } from '../../../types/types';
 
-const costsData: DailyExpectedCost[] = [];
+const costsData: DailyExpectedCost[] = [
+	{
+		country: 'GB',
+		daily_cost_in_dollars: 200,
+	},
+	{
+		country: 'US',
+		daily_cost_in_dollars: 150,
+	},
+	{
+		country: 'TR',
+		daily_cost_in_dollars: 100,
+	},
+	{
+		country: 'DE',
+		daily_cost_in_dollars: 125,
+	},
+];
 
 export default costsData;
