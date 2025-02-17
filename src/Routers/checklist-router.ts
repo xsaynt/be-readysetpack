a0ac5bbe2806288af7a7580861ee9af39b3b2270
xsaynt <alex-price3@hotmail.co.uk>
2025-02-17 16:10:40 +0000

    removed space from import

--- src/Routers/checklist-router.ts
+++ src/Routers/checklist-router.ts
@@ -1,17 +1,20 @@
-import express from "express";
+import express from 'express';
 import {
-    deleteSingleItemFromItems,
-  getSingleChecklist,
-  postChecklist,
-  removeEntireChecklist,
-  updateChecklistItems,
-} from "../Controllers/checklistcontroller";
+	deleteSingleItemFromItems,
+	getSingleChecklist,
+	postChecklist,
+	removeEntireChecklist,
+	updateChecklistItems,
+} from '../Controllers/checklistcontroller';
 
 const checklistRouter = express.Router();
 
-checklistRouter.get("/:user_id/:trip_id", getSingleChecklist);
-checklistRouter.post("/:user_id/:trip_id", postChecklist);
-checklistRouter.patch("/:user_id/:trip_id", updateChecklistItems);
-checklistRouter.patch("/:user_id/:trip_id/delete-item",deleteSingleItemFromItems)
-checklistRouter.delete("/:user_id/:trip_id",removeEntireChecklist)
+checklistRouter.get('/:user_id/:trip_id', getSingleChecklist);
+checklistRouter.post('/:user_id/:trip_id', postChecklist);
+checklistRouter.patch('/:user_id/:trip_id', updateChecklistItems);
+checklistRouter.patch(
+	'/:user_id/:trip_id/delete-item',
+	deleteSingleItemFromItems
+);
+checklistRouter.delete('/:user_id/:trip_id', removeEntireChecklist);
 export default checklistRouter;
