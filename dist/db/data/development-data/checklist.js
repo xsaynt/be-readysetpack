6bc46d053c798a6608dc662db44d2fcaf9f4f9e1
xsaynt <alex-price3@hotmail.co.uk>
2025-02-19 15:05:01 +0000

    reverted changes

--- dist/db/data/development-data/checklist.js
+++ dist/db/data/development-data/checklist.js
@@ -1,10 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const checklistData = [
-    {
-        trip_id: 1,
-        user_id: 1,
-        items: [],
-    },
-];
+const checklistData = [];
 exports.default = checklistData;
--- src/db/data/development-data/checklist.ts
+++ src/db/data/development-data/checklist.ts
@@ -1,11 +1,5 @@
 import { Checklist } from '../../../types/types';
 
-const checklistData: Checklist[] = [
-	{
-		trip_id: 1,
-		user_id: 1,
-		items: [],
-	},
-];
+const checklistData: Checklist[] = [];
 
 export default checklistData;
