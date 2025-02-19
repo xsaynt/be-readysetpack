838bf5422782eac15aec84928317f38a5f04b47f
xsaynt <alex-price3@hotmail.co.uk>
2025-02-19 14:52:08 +0000

    tsc ran to compile

--- dist/db/data/development-data/checklist.js
+++ dist/db/data/development-data/checklist.js
@@ -1,4 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const checklistData = [];
+const checklistData = [
+    {
+        trip_id: 1,
+        user_id: 1,
+        items: [],
+    },
+];
 exports.default = checklistData;
