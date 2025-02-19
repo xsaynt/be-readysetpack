7b7bad6fc42a18c288639f02043589e0d57b6e71
xsaynt <alex-price3@hotmail.co.uk>
2025-02-19 14:47:34 +0000

    backend checklist amended to show correct keys and values

--- src/db/data/development-data/checklist.ts
+++ src/db/data/development-data/checklist.ts
@@ -1,5 +1,11 @@
 import { Checklist } from '../../../types/types';
 
-const checklistData: Checklist[] = [];
+const checklistData: Checklist[] = [
+	{
+		trip_id: 1,
+		user_id: 1,
+		items: [],
+	},
+];
 
 export default checklistData;
